cc58c0e05634b0208c16ba4a42373bdbd90dcfaf
mundoenrique <mundoenrique@homtail.com>
2023-03-17 12:21:05 -0400

    Declaración de varibles

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,1 +1,14 @@
-console.log('Hola mundo desde archivo JS');
+// console.log('Hola mundo desde archivo JS');
+let a = 10;
+var b = 10; // no se debe usar esta forma
+const c = 10;
+
+// c = 20; // no se debe reasignar valor a una constante
+
+// formas de declarar varibales
+let d = 10,
+  e = 10,
+  f = 10;
+let x = d + e;
+
+console.log(x);
