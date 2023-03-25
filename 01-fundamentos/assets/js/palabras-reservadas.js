ceeb68aa8c7a4d674cea90065648d5ade55f03c0
mundoenrique <mundoenrique@homtail.com>
2023-03-25 09:06:52 -0400

    Palabras reservadas

---
+++ 01-fundamentos/assets/js/palabras-reservadas.js
@@ -0,0 +1,13 @@
+let name = 'José';
+let _objeto$123 = 123;
+let precio99_99 = 99.99;
+let jugadorConPuntajeMasAlto = 'Enrique';
+
+class JuegoAnio {}
+/* NO usar */
+// const
+// if
+// try
+// while
+// for ()
+// enum
