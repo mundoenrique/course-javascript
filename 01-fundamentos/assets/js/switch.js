f8e36d71bd5a9d039bf9c48c8e33b16874a8ddf7
mundoenrique <mundoenrique@homtail.com>
2023-03-30 12:56:32 -0400

    Switch

---
+++ 01-fundamentos/assets/js/switch.js
@@ -0,0 +1,28 @@
+const dia = 6; //0: Domingo
+
+switch (dia) {
+  case 0:
+    console.log('Domingo');
+    break;
+  case 1:
+    console.log('Lunes');
+    break;
+  case 2:
+    console.log('Martes');
+    break;
+  case 3:
+    console.log('Miércoles');
+    break;
+  case 4:
+    console.log('Jueves');
+    break;
+  case 5:
+    console.log('Viernes');
+    break;
+  case 6:
+    console.log('Sábado');
+    break;
+  default:
+    console.log('No es un día de la semana');
+    break;
+}
