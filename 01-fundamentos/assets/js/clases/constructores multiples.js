b6d39496a23c21256c827221a05b1603c03cd2c1
mundoenrique <mundoenrique@homtail.com>
2023-04-04 12:50:36 -0400

    Multiples constructores

---
+++ 01-fundamentos/assets/js/clases/constructores multiples.js
@@ -0,0 +1,30 @@
+class Persona {
+  static porObjeto({ nombre, apellido, pais }) {
+    return new Persona(nombre, apellido, pais);
+  }
+
+  constructor(nombre, apellido, pais) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.pais = pais;
+  }
+
+  getInfo() {
+    console.log(`Info ${this.nombre}, ${this.apellido}, ${this.pais}`);
+  }
+}
+
+const nombre1 = 'Melissa',
+  apellido1 = 'Flores',
+  pais1 = 'Honfuras',
+  kike = {
+    nombre: 'Enrique',
+    apellido: 'Peñaloza',
+    pais: 'Venezuela',
+  };
+
+const persona1 = new Persona(nombre1, apellido1, pais1);
+const persona2 = Persona.porObjeto(kike);
+
+persona1.getInfo();
+persona2.getInfo();
