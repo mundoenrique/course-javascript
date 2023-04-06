d6023dd8eff3abd9f72a49f58614f2b0ef424033
mundoenrique <mundoenrique@homtail.com>
2023-04-06 11:19:11 -0400

    Crea metodos del store

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -1,9 +1,9 @@
 import { Todo } from '../todos/todo.model';
 
 const Filters = {
-  All: 'All',
-  Completed: 'Completed',
-  Pendig: 'Pendig',
+  All: 'all',
+  Completed: 'completed',
+  Pendig: 'pendig',
 };
 
 const state = {
@@ -16,6 +16,50 @@ const initStore = () => {
   console.log('initStore :-)');
 };
 
+const loadStore = () => {};
+
+/**
+ * Crea un todo
+ * @param {String} description
+ */
+const addTodo = (description) => {};
+
+/**
+ * toggle dle todo
+ * @param {string} todoId id del todo
+ */
+const toggleTodo = (todoId) => {};
+
+/**
+ * Elimina un todo
+ * @param {String} todoId id del todo
+ */
+const deleteTodo = (todoId) => {};
+
+/**
+ * elimina todos competados
+ * @param {String} todoId id del todo
+ */
+const deleteCompleted = (todoId) => {};
+
+/**
+ * cambia el tipo de filtro de busqueda
+ * @param {String} newFilter tipo de filtro
+ */
+const setFilter = (newFilter = Filters.All) => {};
+
+/**
+ * Obtienen el filtro actual
+ */
+const getCurrentFilter = () => {};
+
 export default {
+  addTodo,
+  deleteCompleted,
+  deleteTodo,
   initStore,
+  loadStore,
+  setFilter,
+  toggleTodo,
+  getCurrentFilter,
 };
