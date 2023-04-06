ca47620dad9d721945279e18d64f2128e02f7790
mundoenrique <mundoenrique@homtail.com>
2023-04-06 14:53:32 -0400

    Crea una tarea

--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -4,6 +4,7 @@ import { renderTodos } from '../use-cases';
 
 const elementIds = {
   todoList: '.todo-list',
+  newTodoInput: '#new-todo-input',
 };
 /**
  *
@@ -21,4 +22,17 @@ export const App = (elementId) => {
     document.querySelector(elementId).append(app);
     displayTodos();
   })();
+
+  // Referencias HTML
+  const newTodoDescrption = document.querySelector(elementIds.newTodoInput);
+
+  // Listeners
+  newTodoDescrption.addEventListener('keyup', (event) => {
+    if (event.keyCode !== 13) return;
+    if (event.target.value.trim().length === 0) return;
+
+    todoStore.addTodo(event.target.value);
+    displayTodos();
+    event.target.value = '';
+  });
 };
