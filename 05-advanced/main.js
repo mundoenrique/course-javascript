2118ba71c6a7583ed10faac02d4770ae1af535fb
mundoenrique <mundoenrique@homtail.com>
2023-04-07 10:49:47 -0400

    if await y for await

--- 05-advanced/main.js
+++ 05-advanced/main.js
@@ -6,8 +6,9 @@ import { callbacksComponent } from './src/concepts/02-callbacks';
 import { promisesComponent } from './src/concepts/03-promises';
 import { promiseRaceComponent } from './src/concepts/04-promise-race';
 import { asyncComponent } from './src/concepts/05-async';
-// import { asyncAwaitComponent } from './src/concepts/06-async-await';
+import { asyncAwaitComponent } from './src/concepts/06-async-await';
 import { asyncAwait2Component } from './src/concepts/07-async-await';
+import { forAwaitComponent } from './src/concepts/08-for-await';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -31,4 +32,5 @@ const element = document.querySelector('.card');
 // promiseRaceComponent(element);
 // asyncComponent(element);
 // asyncAwaitComponent(element);
-asyncAwait2Component(element);
+// asyncAwait2Component(element);
+forAwaitComponent(element);
---
+++ 05-advanced/src/concepts/08-for-await.js
@@ -0,0 +1,46 @@
+import { heroes } from '../data/heroes';
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const forAwaitComponent = async (element) => {
+  // const id = '5d86371fd55e2e2a30fe1ccb1';
+
+  // if (await getHeroAsync(id)) {
+  //   element.innerHTML = 'Si existe ese héroe';
+  //   return;
+  // }
+
+  // element.innerHTML = 'No existe ese héroe';
+
+  const heroIds = heroes.map((hero) => hero.id);
+
+  const heroPromises = getHeroesAsync(heroIds);
+
+  for await (const hero of heroPromises) {
+    element.innerHTML += `${hero.name} <br/>`;
+  }
+};
+
+/**
+ *
+ * @param {Array<String>} heroIds
+ * @returns {Array<Promise>}
+ */
+const getHeroesAsync = (heroIds) => {
+  const heroPromises = [];
+
+  heroIds.forEach((id) => {
+    heroPromises.push(getHeroAsync(id));
+  });
+
+  return heroPromises;
+};
+
+const getHeroAsync = async (id) => {
+  await new Promise((resolve) => {
+    setTimeout(() => resolve(), 1000);
+  });
+
+  return heroes.find((hero) => hero.id === id);
+};
