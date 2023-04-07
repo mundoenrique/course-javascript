c613e7a5a99d98b7d3a600279cea0b3ee01e43d8
mundoenrique <mundoenrique@homtail.com>
2023-04-07 18:21:16 -0400

    Botón para crear usuarios

---
+++ 07-crud-app/src/users/presentation/render-add-button/render-add-button.js
@@ -0,0 +1,14 @@
+import './render-add-button.css';
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const renderAddButton = (element, callback) => {
+  const fabButton = document.createElement('button');
+  fabButton.innerText = '+';
+  fabButton.classList.add('fab-button');
+  element.append(fabButton);
+  fabButton.addEventListener('click', () => {
+    throw Error('No implementado');
+  });
+};
