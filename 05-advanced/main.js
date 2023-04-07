dc754dd1e96967040ab3aa055f50f68ec2daf5a6
mundoenrique <mundoenrique@homtail.com>
2023-04-07 11:57:44 -0400

    Función generadora asíncrona

--- 05-advanced/main.js
+++ 05-advanced/main.js
@@ -10,6 +10,7 @@ import { asyncAwaitComponent } from './src/concepts/06-async-await';
 import { asyncAwait2Component } from './src/concepts/07-async-await';
 import { forAwaitComponent } from './src/concepts/08-for-await';
 import { generatorFunctionComponent } from './src/concepts/09-generators';
+import { generatorAsyncComponent } from './src/concepts/10-generators-async';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -35,4 +36,5 @@ const element = document.querySelector('.card');
 // asyncAwaitComponent(element);
 // asyncAwait2Component(element);
 // forAwaitComponent(element);
-generatorFunctionComponent(element);
+// generatorFunctionComponent(element);
+generatorAsyncComponent(element);
---
+++ 05-advanced/src/concepts/10-generators-async.js
@@ -0,0 +1,36 @@
+import { heroes } from '../data/heroes';
+
+/**
+ *
+ * @param {HTMLDivElement} element elemnto html
+ */
+export const generatorAsyncComponent = async (element) => {
+  const heroGenerator = getHeroGenerator();
+  let isFinished = false;
+
+  do {
+    const { value, done } = await heroGenerator.next();
+    isFinished = done;
+
+    if (isFinished) break;
+
+    element.innerHTML = value;
+  } while (!isFinished);
+};
+
+async function* getHeroGenerator() {
+  for (const hero of heroes) {
+    await sleep();
+    yield hero.name;
+  }
+
+  return 'No hay más';
+}
+
+const sleep = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 500);
+  });
+};
