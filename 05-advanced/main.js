b82f7f2da551f3578ad2599cf95bc16143f4220a
mundoenrique <mundoenrique@homtail.com>
2023-04-07 12:02:38 -0400

    NOmbre de archivo

--- 05-advanced/main.js
+++ 05-advanced/main.js
@@ -1,7 +1,7 @@
 import './style.css';
 import javascriptLogo from './javascript.svg';
 import viteLogo from '/vite.svg';
-import { enviromentComponent } from './src/concepts/01-enviroments';
+import { enviromentComponent } from './src/concepts/01-environments';
 import { callbacksComponent } from './src/concepts/02-callbacks';
 import { promisesComponent } from './src/concepts/03-promises';
 import { promiseRaceComponent } from './src/concepts/04-promise-race';
