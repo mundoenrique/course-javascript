9c2eaa6a46ca3d3c5d0cb4fc373806569b5a3e2b
mundoenrique <mundoenrique@homtail.com>
2023-04-07 19:46:34 -0400

    Crear usuario

---
+++ 07-crud-app/src/users/use-cases/save-user.js
@@ -0,0 +1,36 @@
+import { User } from '../models/users';
+
+/**
+ *
+ * @param {Like<User>} userLike
+ */
+export const saveUser = async (userLike) => {
+  const user = new User(userLike);
+
+  if (user.id) {
+    throw 'No implemantada la actualización';
+    return;
+  }
+
+  const updatedUser = await createUser(user);
+
+  return updatedUser;
+};
+
+/**
+ *
+ * @param {Like<User>} user
+ */
+const createUser = async (user) => {
+  const url = `${import.meta.env.VITE_BASE_URL}/users`;
+  const res = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const newUser = res.json();
+  console.log(newUser);
+  return newUser;
+};
