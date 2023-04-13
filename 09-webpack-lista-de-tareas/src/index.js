7cd033fbe8a94b58b4dd6fd7939b718775a0ef62
mundoenrique <mundoenrique@homtail.com>
2023-04-13 09:28:23 -0400

    Cre una tarea

--- 09-webpack-lista-de-tareas/src/index.js
+++ 09-webpack-lista-de-tareas/src/index.js
@@ -1,5 +1,9 @@
 import './styles.css';
-import { Todo } from './clasess/todo.class';
+import { Todo, TodoList } from './clasess';
 
+const todoList = new TodoList();
 const tarea = new Todo('Aprender Javascript');
-console.log(tarea);
+const tarea2 = new Todo('Comprar un unicornio');
+todoList.nuevoTodo(tarea);
+todoList.nuevoTodo(tarea2);
+console.log(todoList);
