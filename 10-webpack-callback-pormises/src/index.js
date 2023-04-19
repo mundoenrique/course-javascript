61995a84ac950ca9f21d541d3b1186448835ebf9
mundoenrique <mundoenrique@homtail.com>
2023-04-19 15:24:47 -0400

    Ordena código

--- 10-webpack-callback-pormises/src/index.js
+++ 10-webpack-callback-pormises/src/index.js
@@ -5,7 +5,16 @@ const heroeId1 = 'capi';
 const heroeId2 = 'iron';
 let heroInfo1, heroInfo2;
 
-console.log('callback');
+console.warn('callback');
+buscarHeroeCallback(heroeId1, (err, heroe1) => {
+  if (err) {
+    return console.error(err);
+  }
+
+  console.log(`Enviando a ${heroe1.nombre} a la misión`);
+});
+
+console.warn('callback hell');
 buscarHeroeCallback(heroeId1, (err, heroe1) => {
   if (err) {
     return console.error(err);
@@ -20,9 +29,9 @@ buscarHeroeCallback(heroeId1, (err, heroe1) => {
   });
 });
 
-console.log('Promesa');
 buscarHeroePromesa(heroeId1)
   .then((heroe1) => {
+    console.warn('Promesa');
     console.log(`Enviando a ${heroe1.nombre} a la misión`);
   })
   .catch(console.error);
@@ -31,6 +40,7 @@ buscarHeroePromesa(heroeId1)
   .then((heroe1) => {
     buscarHeroePromesa(heroeId2)
       .then((heroe2) => {
+        console.warn('Promesa hell');
         console.log(`Enviando a ${heroe1.nombre} y a ${heroe2.nombre} a la misión`);
       })
       .catch(console.error);
@@ -44,12 +54,14 @@ buscarHeroePromesa(heroeId1)
   })
   .then((heroe) => {
     heroInfo2 = heroe;
+    console.warn('Promesa encadenadas');
     console.log(`Enviando a ${heroInfo1.nombre} y a ${heroInfo2.nombre} a la misión`);
   })
   .catch(console.error);
 
 Promise.all([buscarHeroePromesa(heroeId1), buscarHeroePromesa(heroeId2)])
   .then(([heroe1, heroe2]) => {
+    console.warn('Promesa (all)');
     console.log(`Enviando a ${heroe1.nombre} y a ${heroe2.nombre} a la misión`);
   })
   .catch(console.error);
