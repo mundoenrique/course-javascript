7961bb2dcf22451049a11dcc535272a8ad8381dd
mundoenrique <mundoenrique@homtail.com>
2023-04-19 12:20:59 -0400

    Guarda y obtienen tareas de localstorage

--- 09-webpack-lista-de-tareas/src/clasess/todo-list-class.js
+++ 09-webpack-lista-de-tareas/src/clasess/todo-list-class.js
@@ -1,20 +1,24 @@
 export class TodoList {
   constructor() {
-    this.todos = [];
+    // this.todos = [];
+    this.cargarLocalStorage();
   }
 
   nuevoTodo(todo) {
     this.todos.push(todo);
+    this.guardarLocalStorage();
   }
 
   eliminarTodo(id) {
     this.todos = this.todos.filter((todo) => todo.id != id);
+    this.guardarLocalStorage();
   }
 
   marcarCompletado(id) {
     for (const todo of this.todos) {
       if (todo.id == id) {
         todo.completado = !todo.completado;
+        this.guardarLocalStorage();
         break;
       }
     }
@@ -22,5 +26,14 @@ export class TodoList {
 
   eliminarCompletados() {
     this.todos = this.todos.filter((todo) => !todo.completado);
+    this.guardarLocalStorage();
+  }
+
+  guardarLocalStorage() {
+    localStorage.setItem('todo', JSON.stringify(this.todos));
+  }
+
+  cargarLocalStorage() {
+    this.todos = localStorage.getItem('todo') ? JSON.parse(localStorage.getItem('todo')) : [];
   }
 }
--- 09-webpack-lista-de-tareas/src/index.js
+++ 09-webpack-lista-de-tareas/src/index.js
@@ -3,10 +3,5 @@ import { Todo, TodoList } from './clasess';
 import { crearTodoHtml } from './js/componentes';
 
 export const todoList = new TodoList();
-const tarea = new Todo('Aprender Javascript');
 
-todoList.nuevoTodo(tarea);
-
-console.log(todoList);
-
-crearTodoHtml(tarea);
+todoList.todos.forEach(crearTodoHtml);
