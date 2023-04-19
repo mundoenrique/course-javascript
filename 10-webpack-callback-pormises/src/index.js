77beac23d5841d1560d6878d13e6d7dff7e764d2
mundoenrique <mundoenrique@homtail.com>
2023-04-19 14:28:06 -0400

    callback hell

--- 10-webpack-callback-pormises/src/index.js
+++ 10-webpack-callback-pormises/src/index.js
@@ -1,11 +1,18 @@
 import { buscarHeroe } from './js/callbacks';
 import './styles.css';
-const heroeId = 'capi';
+const heroeId1 = 'capi';
+const heroeId2 = 'iron';
 
-buscarHeroe(heroeId, (err, heroe) => {
+buscarHeroe(heroeId1, (err, heroe1) => {
   if (err) {
-    console.error(err);
-  } else {
-    console.info(heroe);
+    return console.error(err);
   }
+
+  buscarHeroe(heroeId2, (err, heroe2) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    console.log(`Enviando a ${heroe1.nombre} y a ${heroe2.nombre} a la misión`);
+  });
 });
