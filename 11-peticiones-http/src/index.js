d8e4d1d2a61d26a8f3c7a67ef930622df479f33e
mundoenrique <mundoenrique@homtail.com>
2023-04-20 14:01:47 -0400

    Peticiones get, post, delete

--- 11-peticiones-http/src/index.js
+++ 11-peticiones-http/src/index.js
@@ -1,4 +1,19 @@
 // import { init } from './js/chistes-page';
-import { init } from './js/usuarios-page';
+// import { init } from './js/usuarios-page';
+// init();
 
-init();
+import * as CRUD from './js/crud-provider';
+
+CRUD.getUsuario(2).then(console.log);
+
+CRUD.crearUsuario({
+  name: 'Enrique',
+  job: 'Ingeniero',
+}).then(console.log);
+
+CRUD.actulizarUsuario(2, {
+  name: 'Enrique',
+  job: 'Ingeniero',
+}).then(console.log);
+
+CRUD.borrarUsuario(2).then(console.log);
